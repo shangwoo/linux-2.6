b29c09a37c1a8a2b27338d2236f228bc378c21ac
Oleksij Rempel <linux@rempel-privat.de>
2014-06-21 18:10:50 +0200

    9260: sound/soc/asm9260/asm9260-i2s.h
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

---
+++ sound/soc/asm9260/asm9260-i2s.h
@@ -0,0 +1,28 @@
+/*
+ * asm9260-i2s.c  --  ALSA Soc Audio Layer
+ *
+ * Copyright 2005 Wolfson Microelectronics PLC.
+ * Author: Graeme Gregory
+ *         graeme.gregory@wolfsonmicro.com or linux@wolfsonmicro.com
+ *
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of  the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the  License, or (at your
+ *  option) any later version.
+ *
+ *  Revision history
+ *    10th Nov 2006   Initial version.
+ */
+
+#ifndef ASM9260I2S_H_
+#define ASM9260I2S_H_
+
+
+/* Clock dividers */
+#define ASM9260_DIV_MCLK	0
+#define ASM9260_DIV_BCLK	1
+
+
+extern struct snd_soc_dai asm9260_i2s_dai;
+
+#endif /ASM9260I2S_H_*/

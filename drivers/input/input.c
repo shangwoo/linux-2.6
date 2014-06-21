a0628151850ac0edd6b07c76dd6082c3bc8a9589
Oleksij Rempel <linux@rempel-privat.de>
2014-06-21 17:38:38 +0200

    9260: drivers/input/input.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -1379,8 +1379,8 @@ int input_register_device(struct input_dev *dev)
 	if (!dev->rep[REP_DELAY] && !dev->rep[REP_PERIOD]) {
 		dev->timer.data = (long) dev;
 		dev->timer.function = input_repeat_key;
-		dev->rep[REP_DELAY] = 250;
-		dev->rep[REP_PERIOD] = 33;
+		dev->rep[REP_DELAY] = 400;
+		dev->rep[REP_PERIOD] = 400;
 	}
 
 	if (!dev->getkeycode)

b559d0b9106e32b5d8f243f75d988147a6bb8572
Oleksij Rempel <linux@rempel-privat.de>
2014-08-25 13:02:10 +0200

    add initial asm9260_timer.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

---
+++ drivers/clocksource/asm9260_timer.c
@@ -0,0 +1,30 @@
+
+
+#define TIMER0_BASE_ADDRESS         0x80088000
+#define HW_TIMER0_IR                0x80088000  /* Interrupt */
+#define HW_TIMER0_TCR               0x80088010  /* Timer controller */
+#define HW_TIMER0_DIR               0x80088020  /* Direction? */
+#define HW_TIMER0_TC0               0x80088030  /* TImer counter 0 */
+#define HW_TIMER0_TC1               0x80088040
+#define HW_TIMER0_TC2               0x80088050
+#define HW_TIMER0_TC3               0x80088060
+#define HW_TIMER0_PR                0x80088070 /* prescaler */
+#define HW_TIMER0_PC                0x80088080 /* Prescaler counter */
+#define HW_TIMER0_MCR               0x80088090 /* Match control */
+#define HW_TIMER0_MR0               0x800880a0
+#define HW_TIMER0_MR1               0x800880b0
+#define HW_TIMER0_MR2               0x800880C0
+#define HW_TIMER0_MR3               0x800880D0
+#define HW_TIMER0_CCR               0x800880E0  /* Capture control */
+#define HW_TIMER0_CR0               0x800880F0  /* Capture reg */
+#define HW_TIMER0_CR1               0x80088100
+#define HW_TIMER0_CR2               0x80088110
+#define HW_TIMER0_CR3               0x80088120
+#define HW_TIMER0_EMR               0x80088130
+#define HW_TIMER0_PWMTH0            0x80088140
+#define HW_TIMER0_PWMTH1            0x80088150
+#define HW_TIMER0_PWMTH2            0x80088160
+#define HW_TIMER0_PWMTH3            0x80088170
+#define HW_TIMER0_CTCR              0x80088180
+#define HW_TIMER0_PWMC              0x80088190
+

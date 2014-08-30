c83c982bf0745b5af359662eae17f2bb8baacf23
Oleksij Rempel <linux@rempel-privat.de>
2014-08-30 22:49:05 +0200

    drivers/clocksource/asm9260_timer.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/clocksource/asm9260_timer.c
+++ drivers/clocksource/asm9260_timer.c
@@ -81,89 +81,55 @@
 #define HW_MR3		0x00D0
 #define HW_CCR		0x00E0 /* RW. Capture control */
 #define HW_CR0		0x00F0 /* RO. Capture reg */
-#define HW_CR1               0x0100
-#define HW_CR2               0x0110
-#define HW_CR3               0x0120
-#define HW_EMR               0x0130 /* RW. External Match */
-#define HW_PWMTH0            0x0140 /* RW. PWM width */
-#define HW_PWMTH1            0x0150
-#define HW_PWMTH2            0x0160
-#define HW_PWMTH3            0x0170
-#define HW_CTCR              0x0180 /* Counter control */
-#define HW_PWMC              0x0190 /* PWM control */
+#define HW_CR1		0x0100
+#define HW_CR2		0x0110
+#define HW_CR3		0x0120
+#define HW_EMR		0x0130 /* RW. External Match */
+#define HW_PWMTH0	0x0140 /* RW. PWM width */
+#define HW_PWMTH1	0x0150
+#define HW_PWMTH2	0x0160
+#define HW_PWMTH3	0x0170
+#define HW_CTCR		0x0180 /* Counter control */
+#define HW_PWMC		0x0190 /* PWM control */
 
-
-/*
- * ---------------------------------------------------------------------------
- * 32KHz OS timer
- *
- * This currently works only on 16xx, as 1510 does not have the continuous
- * 32KHz synchronous timer. The 32KHz synchronous timer is used to keep track
- * of time in addition to the 32KHz OS timer. Using only the 32KHz OS timer
- * on 1510 would be possible, but the timer would not be as accurate as
- * with the 32KHz synchronized timer.
- * ---------------------------------------------------------------------------
- */
-
-/* 16xx specific defines */
-#define OMAP1_32K_TIMER_BASE		0xfffb9000
-#define OMAP1_32KSYNC_TIMER_BASE	0xfffbc400
-#define OMAP1_32K_TIMER_CR		0x08
-#define OMAP1_32K_TIMER_TVR		0x00
-#define OMAP1_32K_TIMER_TCR		0x04
-
-#define OMAP_32K_TICKS_PER_SEC		(32768)
 
 /*
  * TRM says 1 / HZ = ( TVR + 1) / 32768, so TRV = (32768 / HZ) - 1
  * so with HZ = 128, TVR = 255.
  */
-#define OMAP_32K_TIMER_TICK_PERIOD	((OMAP_32K_TICKS_PER_SEC / HZ) - 1)
+//#define OMAP_32K_TIMER_TICK_PERIOD	((OMAP_32K_TICKS_PER_SEC / HZ) - 1)
 
-#define JIFFIES_TO_HW_TICKS(nr_jiffies, clock_rate)			\
-				(((nr_jiffies) * (clock_rate)) / HZ)
+//#define JIFFIES_TO_HW_TICKS(nr_jiffies, clock_rate)			\
+//				(((nr_jiffies) * (clock_rate)) / HZ)
 
-static void __iomem *timer_base;
+static void __iomem *base;
 
-static inline void omap_32k_timer_write(int val, int reg)
+static inline void asm9260_timer_start(void)
 {
-	omap_writew(val, OMAP1_32K_TIMER_BASE + reg);
+	writel(1 << 0, base + HW_TCR + SET_REG);
 }
 
-static inline unsigned long omap_32k_timer_read(int reg)
+static inline void asm9260_timer_stop(void)
 {
-	return omap_readl(OMAP1_32K_TIMER_BASE + reg) & 0xffffff;
+	writel(1 << 0, base + HW_TCR + CLR_REG);
 }
 
-static inline void omap_32k_timer_start(unsigned long load_val)
-{
-	if (!load_val)
-		load_val = 1;
-	omap_32k_timer_write(load_val, OMAP1_32K_TIMER_TVR);
-	omap_32k_timer_write(0x0f, OMAP1_32K_TIMER_CR);
-}
-
-static inline void omap_32k_timer_stop(void)
-{
-	omap_32k_timer_write(0x0, OMAP1_32K_TIMER_CR);
-}
-
-static int omap_32k_timer_set_next_event(unsigned long delta,
+static int asm9260_timer_set_next_event(unsigned long delta,
 					 struct clock_event_device *dev)
 {
-	omap_32k_timer_start(delta);
+	asm9260_timer_start();
 
 	return 0;
 }
 
-static void omap_32k_timer_set_mode(enum clock_event_mode mode,
+static void asm9260_timer_set_mode(enum clock_event_mode mode,
 				    struct clock_event_device *evt)
 {
-	omap_32k_timer_stop();
+	asm9260_timer_stop();
 
 	switch (mode) {
 	case CLOCK_EVT_MODE_PERIODIC:
-		omap_32k_timer_start(OMAP_32K_TIMER_TICK_PERIOD);
+		asm9260_timer_start();
 		break;
 	case CLOCK_EVT_MODE_ONESHOT:
 	case CLOCK_EVT_MODE_UNUSED:
@@ -174,66 +140,75 @@ static void omap_32k_timer_set_mode(enum clock_event_mode mode,
 	}
 }
 
-static struct clock_event_device clockevent_32k_timer = {
-	.name		= "32k-timer",
-	.features       = CLOCK_EVT_FEAT_PERIODIC | CLOCK_EVT_FEAT_ONESHOT,
-	.set_next_event	= omap_32k_timer_set_next_event,
-	.set_mode	= omap_32k_timer_set_mode,
+static struct clock_event_device asm9260_clockevent_device = {
+	.name		= "asm9260-timer",
+	.rating		= 200,
+	.features       = CLOCK_EVT_FEAT_PERIODIC,
+	.set_next_event	= asm9260_timer_set_next_event,
+	.set_mode	= asm9260_timer_set_mode,
 };
 
-static irqreturn_t omap_32k_timer_interrupt(int irq, void *dev_id)
+static irqreturn_t asm9260_timer_interrupt(int irq, void *dev_id)
 {
-	struct clock_event_device *evt = &clockevent_32k_timer;
+	struct clock_event_device *evt = &asm9260_clockevent_device;
 
 	/* TODO: need to ack timer register */
+	writel(0x00000001, base + HW_IR);
 
 	evt->event_handler(evt);
 
 	return IRQ_HANDLED;
 }
 
-static struct irqaction omap_32k_timer_irq = {
-	.name		= "32KHz timer",
+static struct irqaction asm9260_timer_irq = {
+	.name		= "ASM9260 timer",
 	.flags		= IRQF_TIMER | IRQF_IRQPOLL,
-	.handler	= omap_32k_timer_interrupt,
+	.handler	= asm9260_timer_interrupt,
 };
-
-static __init void omap_init_32k_timer(void)
-{
-	setup_irq(INT_OS_TIMER, &omap_32k_timer_irq);
-
-	clockevent_32k_timer.cpumask = cpumask_of(0);
-	clockevents_config_and_register(&clockevent_32k_timer,
-					OMAP_32K_TICKS_PER_SEC, 1, 0xfffffffe);
-}
 
 /*
  * ---------------------------------------------------------------------------
  * Timer initialization
  * ---------------------------------------------------------------------------
  */
-int __init omap_32k_timer_init(void)
+int __init asm9260_timer_init(void)
 {
+	int irq;
+	struct clk *clk;
 	int ret = -ENODEV;
 
 	of_address_to_resource(np, 0, &res);
 	if (!request_mem_region(res.start, resource_size(&res), np->name))
 		panic("%s: unable to request mem region", np->name);
 
-	icoll_base = ioremap_nocache(res.start, resource_size(&res));
-	if (!icoll_base)
+	base = ioremap_nocache(res.start, resource_size(&res));
+	if (!base)
 		panic("%s: unable to map resource", np->name);
 
 	clk = of_clk_get(np, 0);
 
-	err = clk_prepare_enable(port->clk_ahb);
+	err = clk_prepare_enable(clk);
 	if (err)
-		dev_err(uport->dev, "Failed to enable ahb_clk!\n");
+		panic(uport->dev, "Failed to enable clk!\n");
 
-	ret = omap_init_clocksource_32k(base);
+	irq = irq_of_parse_and_map(np, 0);
+	setup_irq(irq, &mxs_timer_irq);
 
-	if (!ret)
-		omap_init_32k_timer();
+	/* timer0 count-Up */
+	writel(0x3, base + HW_DIR + CLR_REG);
+	writel(clk_get_rate(clk), base + HW_MR0);
+	/* MR0 * (PR + 1) = hclk * 10000----100HZ-->hclk M */
+	writel(9999, base + HW_PR);
+	/* timer mode */
+	writel(0x3, base + HW_CTCR + CLR_REG);
+	/* if (tc == mr0) then reset tc and interrupt */
+	writel(0x3, base + HW_MCR + SET_REG);
+
+	asm9260_clockevent_device.cpumask = cpumask_of(0);
+	clockevents_config_and_register(&asm9260_clockevent_device,
+					100 * HZ, 1, 0xfffffffe);
 
 	return ret;
 }
+CLOCKSOURCE_OF_DECLARE(asm9260-timer, "alpscale,asm9260-timer",
+		asm9260_timer_init);

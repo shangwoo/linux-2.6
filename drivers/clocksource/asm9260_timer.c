4608486d192104d26caf4ba77f99c18944c53bca
Oleksij Rempel <linux@rempel-privat.de>
2014-09-01 22:08:49 +0200

    drivers/clocksource/asm9260_timer.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/clocksource/asm9260_timer.c
+++ drivers/clocksource/asm9260_timer.c
@@ -1,20 +1,26 @@
 /*
  * Copyright (C) 2014 Oleksij Rempel <linux@rempel-privat.de>
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
  *
  */
 
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/delay.h>
 #include <linux/interrupt.h>
 #include <linux/sched.h>
-#include <linux/spinlock.h>
-#include <linux/err.h>
 #include <linux/clk.h>
 #include <linux/clocksource.h>
 #include <linux/clockchips.h>
@@ -23,8 +29,6 @@
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/bitops.h>
-
-#include <asm/irq.h>
 
 #define SET_REG 4
 #define CLR_REG 8

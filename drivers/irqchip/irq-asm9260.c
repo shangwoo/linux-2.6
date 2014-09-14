200c2fb1fe34a6f9b0165adc8f0aa92b268223b7
Oleksij Rempel <linux@rempel-privat.de>
2014-09-14 14:29:33 +0200

    drivers/irqchip/irq-asm9260.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/irqchip/irq-asm9260.c
+++ drivers/irqchip/irq-asm9260.c
@@ -34,7 +34,8 @@
 #define CLR_REG 8
 
 #define HW_ICOLL_VECTOR				0x0000
-/* bits 31:2
+/*
+ * bits 31:2
  * This register presents the vector address for the interrupt currently
  * active on the CPU IRQ input. Writing to this register notifies the
  * interrupt collector that the interrupt service routine for the current
@@ -43,23 +44,24 @@
  * LDPC HW_ICOLL_VECTOR_ADDR; IRQ exception at 0xffff0018
  */
 
-#define HW_ICOLL_LEVELACK			0x0010
-/* The Interrupt Collector Level Acknowledge Register is used by software to
+/*
+ * The Interrupt Collector Level Acknowledge Register is used by software to
  * indicate the completion of an interrupt on a specific level.
  * This register is written at the very end of an interrupt service routine. If
  * nesting is used then the CPU irq must be turned on before writing to this
  * register to avoid a race condition in the CPU interrupt hardware.
  */
+#define HW_ICOLL_LEVELACK			0x0010
 #define BM_LEVELn(nr)				BIT(nr)
 
 #define HW_ICOLL_CTRL				0x0020
 /* BM_CTRL_SFTRST and BM_CTRL_CLKGATE are not available on asm9260. */
 #define BM_CTRL_SFTRST				BIT(31)
 #define BM_CTRL_CLKGATE				BIT(30)
-#define BM_CTRL_NO_NESTING			BIT(19)
 /* disable interrupt level nesting */
-#define BM_CTRL_ARM_RSE_MODE			BIT(18)
-/* Set this bit to one enable the RISC32-style read side effect associated with
+#define BM_CTRL_NO_NESTING			BIT(19)
+/*
+ * Set this bit to one enable the RISC32-style read side effect associated with
  * the vector address register. In this mode, interrupt in-service is signaled
  * by the read of the HW_ICOLL_VECTOR register to acquire the interrupt vector
  * address. Set this bit to zero for normal operation, in which the ISR signals
@@ -67,20 +69,23 @@
  * 0 - Must Write to Vector register to go in-service.
  * 1 - Go in-service as a read side effect
  */
+#define BM_CTRL_ARM_RSE_MODE			BIT(18)
 #define BM_CTRL_IRQ_ENABLE			BIT(16)
 
 #define HW_ICOLL_STAT_OFFSET			0x0030
-/* bits 5:0
+/*
+ * bits 5:0
  * Vector number of current interrupt. Multiply by 4 and add to vector base
  * address to obtain the value in HW_ICOLL_VECTOR.
  */
 
-#define HW_ICOLL_RAW0				0x0040
-#define HW_ICOLL_RAW1				0x0050
-/* This register provides a read-only view of the raw interrupt request lines
+/*
+ * RAW0 and RAW1 provides a read-only view of the raw interrupt request lines
  * coming from various parts of the chip. Its purpose is to improve diagnostic
  * observability.
  */
+#define HW_ICOLL_RAW0				0x0040
+#define HW_ICOLL_RAW1				0x0050
 
 #define	HW_ICOLL_INTERRUPT0			0x0060
 #define	HW_ICOLL_INTERRUPTn(n)			(0x0060 + ((n) >> 2) * 0x10)
@@ -88,9 +93,11 @@
 		+ SET_REG)
 #define	HW_ICOLL_INTERRUPTn_CLR(n)		(HW_ICOLL_INTERRUPTn(n) \
 		+ CLR_REG)
+/*
+ * WARNING: Modifying the priority of an enabled interrupt may result in
+ * undefined behavior.
+ */
 #define BM_INT_PRIORITY_MASK			0x3
-/* WARNING: Modifying the priority of an enabled interrupt may result in
- * undefined behavior. */
 #define BM_INT_ENABLE				BIT(2)
 #define BM_INT_SOFTIRQ				BIT(3)
 
@@ -99,9 +106,11 @@
 			BM_ICOLL_INTERRUPTn_SHIFT(n)))
 
 #define HW_ICOLL_VBASE				0x0160
-/* bits 31:2
+/*
+ * bits 31:2
  * This bitfield holds the upper 30 bits of the base address of the vector
- * table. */
+ * table.
+ */
 
 #define HW_ICOLL_CLEAR0				0x01d0
 #define	HW_ICOLL_CLEAR1				0x01e0
@@ -118,6 +127,7 @@ static void __iomem *icoll_base;
 static struct irq_domain *icoll_domain;
 static int use_cached_level = 1;
 static u8 level_cache[ICOLL_NUM_IRQS];
+static DEFINE_MUTEX(icoll_lock);
 
 static unsigned int irq_get_level(struct irq_data *d)
 {
@@ -159,14 +169,17 @@ static void icoll_unmask_irq(struct irq_data *d)
 {
 	u32 level;
 
+	level = irq_get_level(d);
+
+	mutex_lock(&icoll_lock);
 	writel_relaxed(BM_CLEAR_BIT(d->hwirq),
 			icoll_base + HW_ICOLL_CLEARn(d->hwirq));
 
-	level = irq_get_level(d);
 	writel_relaxed(BM_LEVELn(level), icoll_base + HW_ICOLL_LEVELACK);
 
 	writel_relaxed(BM_ICOLL_INTERRUPTn_ENABLE(d->hwirq),
 			icoll_base + HW_ICOLL_INTERRUPTn_SET(d->hwirq));
+	mutex_unlock(&icoll_lock);
 }
 
 static struct irq_chip asm9260_icoll_chip = {
@@ -177,10 +190,13 @@ static struct irq_chip asm9260_icoll_chip = {
 
 asmlinkage void __exception_irq_entry icoll_handle_irq(struct pt_regs *regs)
 {
-	u32 irqnr;
+	u32 irqnr, hwirq;
 
-	irqnr = irq_find_mapping(icoll_domain,
-			readl_relaxed(icoll_base + HW_ICOLL_STAT_OFFSET));
+	hwirq = readl_relaxed(icoll_base + HW_ICOLL_STAT_OFFSET);
+	irqnr = irq_find_mapping(icoll_domain, hwirq);
+
+	if (unlikely(!irqnr))
+		panic("Can't find irqnr for hwirq %i", hwirq);
 
 	handle_IRQ(irqnr, regs);
 }
@@ -205,20 +221,24 @@ static int __init icoll_of_init(struct device_node *np,
 	struct resource res;
 	int i;
 
-	of_address_to_resource(np, 0, &res);
+	if (of_address_to_resource(np, 0, &res))
+		panic("%s: unable to get resource", np->full_name);
+
 	if (!request_mem_region(res.start, resource_size(&res), np->name))
-		panic("%s: unable to request mem region", np->name);
+		panic("%s: unable to request mem region", np->full_name);
 
 	icoll_base = ioremap_nocache(res.start, resource_size(&res));
 	if (!icoll_base)
-		panic("%s: unable to map resource", np->name);
+		panic("%s: unable to map resource", np->full_name);
 
 	/* enable IRQ controller */
 	writel_relaxed(BM_CTRL_ARM_RSE_MODE | BM_CTRL_IRQ_ENABLE,
 			icoll_base + HW_ICOLL_CTRL);
 
-	/* This ICOLL has no reset option. So, set all priorities
-	 * manually to 0. */
+	/*
+	 * This ICOLL has no reset option. So, set all priorities
+	 * manually to 0.
+	 */
 	for (i = 0; i < 16 * 0x10; i += 0x10)
 		writel(0, icoll_base + HW_ICOLL_INTERRUPT0 + i);
 
@@ -227,6 +247,8 @@ static int __init icoll_of_init(struct device_node *np,
 
 	icoll_domain = irq_domain_add_linear(np, ICOLL_NUM_IRQS,
 					     &icoll_irq_domain_ops, NULL);
+	if (!icoll_domain)
+		panic("%s: unable add irq domain", np->full_name);
 
 	irq_set_default_host(icoll_domain);
 

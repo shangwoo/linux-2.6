3d64bcd65d796ad27bb5219cf0faa204fd93e873
Oleksij Rempel <linux@rempel-privat.de>
2014-09-20 10:29:16 +0200

    drivers/clk/clk-asm9260.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/clk/clk-asm9260.c
+++ drivers/clk/clk-asm9260.c
@@ -74,9 +74,9 @@ struct asm9260_gate_data {
 	unsigned int idx;
 	const char *name;
 	const char *parent_name;
-	unsigned long flags;
 	u32 reg;
 	u8 bit_idx;
+	unsigned long flags;
 };
 
 struct asm9260_mux_clock {
@@ -107,12 +107,10 @@ static const char *clk_names[] = {
 static const struct asm9260_div_clk asm9260_div_clks[] __initconst = {
 	{ CLKID_SYS_CPU,	"cpu_div", "main_gate", HW_CPUCLKDIV },
 	{ CLKID_SYS_AHB,	"ahb_div", "cpu_div", HW_SYSAHBCLKDIV },
-#if 0
-	{ CLKID_SYS_I2S0M,"i2s0m_div", "i2s0_mclk",  HW_I2S0MCLKDIV },
-	{ CLKID_SYS_I2S0S,"i2s0s_div", "i2s0_mux",  HW_I2S0SCLKDIV },
-	{ CLKID_SYS_I2S1M,"i2s1m_div", "i2s1_mclk",  HW_I2S1MCLKDIV },
-	{ CLKID_SYS_I2S1S,"i2s1s_div", "i2s1_mux",  HW_I2S1SCLKDIV },
-#endif
+	{ CLKID_SYS_I2S0M,	"i2s0m_div", "i2s0_mclk",  HW_I2S0MCLKDIV },
+	{ CLKID_SYS_I2S0S,	"i2s0s_div", "i2s0_gate",  HW_I2S0SCLKDIV },
+	{ CLKID_SYS_I2S1M,	"i2s1m_div", "i2s1_mclk",  HW_I2S1MCLKDIV },
+	{ CLKID_SYS_I2S1S,	"i2s1s_div", "i2s0_gate",  HW_I2S1SCLKDIV },
 	{ CLKID_SYS_UART0,	"uart0_div", "uart_gate", HW_UART0CLKDIV },
 	{ CLKID_SYS_UART1,	"uart1_div", "uart_gate", HW_UART1CLKDIV },
 	{ CLKID_SYS_UART2,	"uart2_div", "uart_gate", HW_UART2CLKDIV },
@@ -126,74 +124,72 @@ static const struct asm9260_div_clk asm9260_div_clks[] __initconst = {
 };
 
 static const struct asm9260_gate_data asm9260_mux_gates[] __initconst = {
-	{ 0, "main_gate",	"main_mux",	CLK_SET_RATE_PARENT,	HW_MAINCLKUEN,	0 },
-	{ 0, "uart_gate",	"uart_mux",	CLK_SET_RATE_PARENT,	HW_UARTCLKUEN,	0 },
-	{ 0, "i2s0_gate",	"i2s0_mux",	CLK_SET_RATE_PARENT,	HW_I2S0CLKUEN,	0 },
-	{ 0, "i2s1_gate",	"i2s1_mux",	CLK_SET_RATE_PARENT,	HW_I2S1CLKUEN,	0 },
-	{ 0, "wdt_gate",	"wdt_mux",	CLK_SET_RATE_PARENT,	HW_WDTCLKUEN,	0 },
-	{ 0, "clkout_gate",	"clkout_mux",	CLK_SET_RATE_PARENT,	HW_CLKOUTCLKUEN, 0 },
+	{ 0, "main_gate",	"main_mux",	HW_MAINCLKUEN,	0 },
+	{ 0, "uart_gate",	"uart_mux",	HW_UARTCLKUEN,	0 },
+	{ 0, "i2s0_gate",	"i2s0_mux",	HW_I2S0CLKUEN,	0 },
+	{ 0, "i2s1_gate",	"i2s1_mux",	HW_I2S1CLKUEN,	0 },
+	{ 0, "wdt_gate",	"wdt_mux",	HW_WDTCLKUEN,	0 },
+	{ 0, "clkout_gate",	"clkout_mux",	HW_CLKOUTCLKUEN, 0 },
 };
 static const struct asm9260_gate_data asm9260_ahb_gates[] __initconst = {
 	/* ahb gates */
-#if 0
-	{ CLKID_AHB_ROM,	"rom",	"ahb_div",	CLK_IGNORE_UNUSED,	HW_AHBCLKCTRL0,	1 },
-	{ CLKID_AHB_RAM,	"ram",	"ahb_div",	CLK_IGNORE_UNUSED,	HW_AHBCLKCTRL0,	2 },
-	{ CLKID_AHB_GPIO,	"gpio",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	4 },
-	{ CLKID_AHB_MAC,	"mac",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	5 },
-	{ CLKID_AHB_EMI,	"emi",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	6 },
-	{ CLKID_AHB_USB0,	"usb0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	7 },
-	{ CLKID_AHB_USB1,	"usb1",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	8 },
-	{ CLKID_AHB_DMA0,	"dma0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	9 },
-	{ CLKID_AHB_DMA1,	"dma1",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	10 },
-#endif
-	{ CLKID_AHB_UART0,	"uart0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	11 },
-	{ CLKID_AHB_UART1,	"uart1",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	12 },
-	{ CLKID_AHB_UART2,	"uart2",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	13 },
-	{ CLKID_AHB_UART3,	"uart3",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	14 },
-	{ CLKID_AHB_UART4,	"uart4",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	15 },
-	{ CLKID_AHB_UART5,	"uart5",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	16 },
-	{ CLKID_AHB_UART6,	"uart6",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	17 },
-	{ CLKID_AHB_UART7,	"uart7",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	18 },
-	{ CLKID_AHB_UART8,	"uart8",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	19 },
-	{ CLKID_AHB_UART9,	"uart9",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	20 },
-#if 0
-	{ CLKID_AHB_I2S0,	"i2s0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	21 },
-	{ CLKID_AHB_I2C0,	"i2c0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	22 },
-	{ CLKID_AHB_I2C1,	"i2c1",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	23 },
-	{ CLKID_AHB_SSP0,	"ssp0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	24 },
-	{ CLKID_AHB_IOCONFIG,	"ioconf",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	25 },
-	{ CLKID_AHB_WDT,	"wdt",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	26 },
-	{ CLKID_AHB_CAN0,	"can0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	27 },
-	{ CLKID_AHB_CAN1,	"can1",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	28 },
-	{ CLKID_AHB_MPWM,	"mpwm",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	29 },
-	{ CLKID_AHB_SPI0,	"spi0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	30 },
-	{ CLKID_AHB_SPI1,	"spi1",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL0,	31 },
+	{ CLKID_AHB_ROM,	"rom",	"ahb_div",	HW_AHBCLKCTRL0,	1,
+		CLK_IGNORE_UNUSED},
+	{ CLKID_AHB_RAM,	"ram",	"ahb_div",	HW_AHBCLKCTRL0,	2,
+		CLK_IGNORE_UNUSED},
+	{ CLKID_AHB_GPIO,	"gpio",	"ahb_div",	HW_AHBCLKCTRL0,	4 },
+	{ CLKID_AHB_MAC,	"mac",	"ahb_div",	HW_AHBCLKCTRL0,	5 },
+	{ CLKID_AHB_EMI,	"emi",	"ahb_div",	HW_AHBCLKCTRL0,	6,
+		CLK_IGNORE_UNUSED},
+	{ CLKID_AHB_USB0,	"usb0",	"ahb_div",	HW_AHBCLKCTRL0,	7 },
+	{ CLKID_AHB_USB1,	"usb1",	"ahb_div",	HW_AHBCLKCTRL0,	8 },
+	{ CLKID_AHB_DMA0,	"dma0",	"ahb_div",	HW_AHBCLKCTRL0,	9 },
+	{ CLKID_AHB_DMA1,	"dma1",	"ahb_div",	HW_AHBCLKCTRL0,	10 },
+	{ CLKID_AHB_UART0,	"uart0",	"ahb_div",	HW_AHBCLKCTRL0,	11 },
+	{ CLKID_AHB_UART1,	"uart1",	"ahb_div",	HW_AHBCLKCTRL0,	12 },
+	{ CLKID_AHB_UART2,	"uart2",	"ahb_div",	HW_AHBCLKCTRL0,	13 },
+	{ CLKID_AHB_UART3,	"uart3",	"ahb_div",	HW_AHBCLKCTRL0,	14 },
+	{ CLKID_AHB_UART4,	"uart4",	"ahb_div",	HW_AHBCLKCTRL0,	15 },
+	{ CLKID_AHB_UART5,	"uart5",	"ahb_div",	HW_AHBCLKCTRL0,	16 },
+	{ CLKID_AHB_UART6,	"uart6",	"ahb_div",	HW_AHBCLKCTRL0,	17 },
+	{ CLKID_AHB_UART7,	"uart7",	"ahb_div",	HW_AHBCLKCTRL0,	18 },
+	{ CLKID_AHB_UART8,	"uart8",	"ahb_div",	HW_AHBCLKCTRL0,	19 },
+	{ CLKID_AHB_UART9,	"uart9",	"ahb_div",	HW_AHBCLKCTRL0,	20 },
+	{ CLKID_AHB_I2S0,	"i2s0",	"ahb_div",		HW_AHBCLKCTRL0,	21 },
+	{ CLKID_AHB_I2C0,	"i2c0",	"ahb_div",		HW_AHBCLKCTRL0,	22 },
+	{ CLKID_AHB_I2C1,	"i2c1",	"ahb_div",		HW_AHBCLKCTRL0,	23 },
+	{ CLKID_AHB_SSP0,	"ssp0",	"ahb_div",		HW_AHBCLKCTRL0,	24 },
+	{ CLKID_AHB_IOCONFIG,	"ioconf",	"ahb_div",	HW_AHBCLKCTRL0,	25 },
+	{ CLKID_AHB_WDT,	"wdt",	"ahb_div",	HW_AHBCLKCTRL0,	26 },
+	{ CLKID_AHB_CAN0,	"can0",	"ahb_div",	HW_AHBCLKCTRL0,	27 },
+	{ CLKID_AHB_CAN1,	"can1",	"ahb_div",	HW_AHBCLKCTRL0,	28 },
+	{ CLKID_AHB_MPWM,	"mpwm",	"ahb_div",	HW_AHBCLKCTRL0,	29 },
+	{ CLKID_AHB_SPI0,	"spi0",	"ahb_div",	HW_AHBCLKCTRL0,	30 },
+	{ CLKID_AHB_SPI1,	"spi1",	"ahb_div",		HW_AHBCLKCTRL0,	31 },
 
-	{ CLKID_AHB_QEI,	"qei",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	0 },
-	{ CLKID_AHB_QUADSPI0,	"quadspi0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	1 },
-	{ CLKID_AHB_CAMIF,	"capmif",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	2 },
-	{ CLKID_AHB_LCDIF,	"lcdif",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	3 },
-#endif
-	{ CLKID_AHB_TIMER0,	"timer0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	4 },
-#if 0
-	{ CLKID_AHB_TIMER1,	"timer1",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	5 },
-	{ CLKID_AHB_TIMER2,	"timer2",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	6 },
-	{ CLKID_AHB_TIMER3,	"timer3",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	7 },
-	{ CLKID_AHB_IRQ,	"irq",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	8 },
-	{ CLKID_AHB_RTC,	"rtc",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	9 },
-	{ CLKID_AHB_NAND,	"nand",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	10 },
-	{ CLKID_AHB_ADC0,	"adc0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	11 },
-	{ CLKID_AHB_LED,	"led",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	12 },
-	{ CLKID_AHB_DAC0,	"dac0",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	13 },
-	{ CLKID_AHB_LCD,	"lcd",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	14 },
-	{ CLKID_AHB_I2S1,	"i2s1",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	15 },
-	{ CLKID_AHB_MAC1,	"mac1",	"ahb_div",	CLK_SET_RATE_PARENT,	HW_AHBCLKCTRL1,	16 },
-#endif
+	{ CLKID_AHB_QEI,	"qei",	"ahb_div",		HW_AHBCLKCTRL1,	0 },
+	{ CLKID_AHB_QUADSPI0,	"quadspi0",	"ahb_div",	HW_AHBCLKCTRL1,	1 },
+	{ CLKID_AHB_CAMIF,	"capmif",	"ahb_div",	HW_AHBCLKCTRL1,	2 },
+	{ CLKID_AHB_LCDIF,	"lcdif",	"ahb_div",	HW_AHBCLKCTRL1,	3 },
+	{ CLKID_AHB_TIMER0,	"timer0",	"ahb_div",	HW_AHBCLKCTRL1,	4 },
+	{ CLKID_AHB_TIMER1,	"timer1",	"ahb_div",	HW_AHBCLKCTRL1,	5 },
+	{ CLKID_AHB_TIMER2,	"timer2",	"ahb_div",	HW_AHBCLKCTRL1,	6 },
+	{ CLKID_AHB_TIMER3,	"timer3",	"ahb_div",	HW_AHBCLKCTRL1,	7 },
+	{ CLKID_AHB_IRQ,	"irq",	"ahb_div",	HW_AHBCLKCTRL1,	8 ,
+		CLK_IGNORE_UNUSED},
+	{ CLKID_AHB_RTC,	"rtc",	"ahb_div",	HW_AHBCLKCTRL1,	9 },
+	{ CLKID_AHB_NAND,	"nand",	"ahb_div",	HW_AHBCLKCTRL1,	10 },
+	{ CLKID_AHB_ADC0,	"adc0",	"ahb_div",	HW_AHBCLKCTRL1,	11 },
+	{ CLKID_AHB_LED,	"led",	"ahb_div",	HW_AHBCLKCTRL1,	12 },
+	{ CLKID_AHB_DAC0,	"dac0",	"ahb_div",	HW_AHBCLKCTRL1,	13 },
+	{ CLKID_AHB_LCD,	"lcd",	"ahb_div",	HW_AHBCLKCTRL1,	14 },
+	{ CLKID_AHB_I2S1,	"i2s1",	"ahb_div",	HW_AHBCLKCTRL1,	15 },
+	{ CLKID_AHB_MAC1,	"mac1",	"ahb_div",	HW_AHBCLKCTRL1,	16 },
 };
 
 static const char *main_mux_p[] = { "oscillator", "pll"};
-static const char *i2s0_mux_p[] = { "oscillator", "pll", "i2s0_mclk_input"};
-static const char *i2s1_mux_p[] = { "oscillator", "pll", "i2s1_mclk_input"};
+static const char *i2s0_mux_p[] = { "oscillator", "pll", "i2s0m_div"};
+static const char *i2s1_mux_p[] = { "oscillator", "pll", "i2s1m_div"};
 static const char *clkout_mux_p[] = { "oscillator", "pll", "rtc"};
 static u32 three_mux_table[] = {0, 1, 3};
 
@@ -250,8 +246,8 @@ static void __init asm9260_pll_init(struct device_node *np)
                 const struct asm9260_gate_data *gd = &asm9260_mux_gates[n];
 
                 clk = clk_register_gate(NULL, gd->name,
-                            gd->parent_name, gd->flags, base + gd->reg,
-                            gd->bit_idx, 0, &asm9260_clk_lock);
+                            gd->parent_name, gd->flags | CLK_SET_RATE_PARENT,
+			    base + gd->reg, gd->bit_idx, 0, &asm9260_clk_lock);
         }
 
 

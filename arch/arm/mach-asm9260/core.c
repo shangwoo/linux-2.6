68a0906a30403febec4677448c76aa431b4c2b2e
Oleksij Rempel <linux@rempel-privat.de>
2014-09-24 11:02:48 +0200

    arch/arm/mach-asm9260/core.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- arch/arm/mach-asm9260/core.c
+++ arch/arm/mach-asm9260/core.c
@@ -22,33 +22,6 @@
 
 #include <linux/of_platform.h>
 #include <asm/mach/arch.h>
-#include <asm/mach/map.h>
-
-static struct map_desc asm9260_io_desc[] __initdata = {
-	{	/* IO space */
-		.virtual	= (unsigned long)0xf0000000,
-		.pfn		= __phys_to_pfn(0x80000000),
-		.length		= 0x00800000,
-		.type		= MT_DEVICE
-	},
-	{	/* LCD IO space	*/
-		.virtual	= (unsigned long)0xf0a00000,
-		.pfn		= __phys_to_pfn(0x80800000),
-		.length		= 0x00009000,
-		.type		= MT_DEVICE
-	},
-	{	/* GPIO IO space */
-		.virtual	= (unsigned long)0xf0800000,
-		.pfn		= __phys_to_pfn(0x50000000),
-		.length		= 0x00100000,
-		.type		= MT_DEVICE
-	},
-};
-
-static void __init asm9260_map_io(void)
-{
-	iotable_init(asm9260_io_desc, ARRAY_SIZE(asm9260_io_desc));
-}
 
 static const char * const asm9260_dt_board_compat[] __initconst = {
 	"alphascale,asm9260",
@@ -56,6 +29,5 @@ static const char * const asm9260_dt_board_compat[] __initconst = {
 };
 
 DT_MACHINE_START(ASM9260, "Alphascale ASM9260 (Device Tree Support)")
-	.map_io		= asm9260_map_io,
 	.dt_compat	= asm9260_dt_board_compat,
 MACHINE_END

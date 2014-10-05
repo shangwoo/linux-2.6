06be12eab6fda395f553bfe0a4fc70dd0782a835
Oleksij Rempel <linux@rempel-privat.de>
2014-10-05 14:59:15 +0200

    arch/arm/mach-asm9260/core.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- arch/arm/mach-asm9260/core.c
+++ arch/arm/mach-asm9260/core.c
@@ -1,23 +1,10 @@
 /*
  * Copyright (C) 2014 Oleksij Rempel <linux@rempel-privat.de>
- *  Co-author: Du Huanpeng <u74147@gmail.com>
- * map_desc based on:
- *  linux/arch/arm/mach-asm9260/core.c
- *  Copyright (C) 2011-2014 Alphascale
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #include <linux/of_platform.h>

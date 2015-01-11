15971d85063b632b6159d7361b7321a048a5d325
Oleksij Rempel <linux@rempel-privat.de>
2015-01-11 18:46:49 +0100

    drivers/watchdog/asm9260_wdt.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/watchdog/asm9260_wdt.c
+++ drivers/watchdog/asm9260_wdt.c
@@ -1,7 +1,7 @@
 /*
- * Watchdog driver for CSR SiRFprimaII and SiRFatlasVI
+ * Watchdog driver for Alphascale ASM9260. Same WD can be found on NXP LPC176x.
  *
- * Copyright (c) 2013 Cambridge Silicon Radio Limited, a CSR plc group company.
+ * Copyright (c) 2014 Oleksij Rempel <linux@rempel-privat.de>
  *
  * Licensed under GPLv2 or later.
  */
@@ -15,6 +15,29 @@
 #include <linux/uaccess.h>
 
 #define CLOCK_FREQ	1000000
+
+/* Watchdog Mode register */
+#define HW_WDMOD			0x00
+#define BM_MOD_WDINT			BIT(3)
+#define BM_MOD_WDTOF			BIT(2)
+#define BM_MOD_WDRESET			BIT(1)
+#define BM_MOD_WDEN			BIT(0)
+
+/*
+ * Watchdog Timer Constant register
+ * Minimal value is 0xff, the meaning of this value
+ * depends on used clock: T = WDCLK * (0xff + 1) * 4
+ */
+#define HW_WDTC				0x04
+#define HW_WDTC				0x04
+
+/* Watchdog Feed register */
+#define HW_WDFEED			0x08
+
+/* Watchdog Timer Value register */
+#define HW_WDTV				0x0c
+
+
 
 #define SIRFSOC_TIMER_COUNTER_LO	0x0000
 #define SIRFSOC_TIMER_MATCH_0		0x0008
@@ -39,7 +62,7 @@ MODULE_PARM_DESC(timeout, "Default watchdog timeout (in seconds)");
 MODULE_PARM_DESC(nowayout, "Watchdog cannot be stopped once started (default="
 			__MODULE_STRING(WATCHDOG_NOWAYOUT) ")");
 
-static unsigned int sirfsoc_wdt_gettimeleft(struct watchdog_device *wdd)
+static unsigned int asm9260_wdt_gettimeleft(struct watchdog_device *wdd)
 {
 	u32 counter, match;
 	void __iomem *wdt_base;
@@ -55,7 +78,7 @@ static unsigned int sirfsoc_wdt_gettimeleft(struct watchdog_device *wdd)
 	return time_left / CLOCK_FREQ;
 }
 
-static int sirfsoc_wdt_updatetimeout(struct watchdog_device *wdd)
+static int asm9260_wdt_updatetimeout(struct watchdog_device *wdd)
 {
 	u32 counter, timeout_ticks;
 	void __iomem *wdt_base;
@@ -77,10 +100,10 @@ static int sirfsoc_wdt_updatetimeout(struct watchdog_device *wdd)
 	return 0;
 }
 
-static int sirfsoc_wdt_enable(struct watchdog_device *wdd)
+static int asm9260_wdt_enable(struct watchdog_device *wdd)
 {
 	void __iomem *wdt_base = watchdog_get_drvdata(wdd);
-	sirfsoc_wdt_updatetimeout(wdd);
+	asm9260_wdt_updatetimeout(wdd);
 
 	/*
 	 * NOTE: If interrupt is not enabled
@@ -94,7 +117,7 @@ static int sirfsoc_wdt_enable(struct watchdog_device *wdd)
 	return 0;
 }
 
-static int sirfsoc_wdt_disable(struct watchdog_device *wdd)
+static int asm9260_wdt_disable(struct watchdog_device *wdd)
 {
 	void __iomem *wdt_base = watchdog_get_drvdata(wdd);
 
@@ -106,40 +129,40 @@ static int sirfsoc_wdt_disable(struct watchdog_device *wdd)
 	return 0;
 }
 
-static int sirfsoc_wdt_settimeout(struct watchdog_device *wdd, unsigned int to)
+static int asm9260_wdt_settimeout(struct watchdog_device *wdd, unsigned int to)
 {
 	wdd->timeout = to;
-	sirfsoc_wdt_updatetimeout(wdd);
+	asm9260_wdt_updatetimeout(wdd);
 
 	return 0;
 }
 
 #define OPTIONS (WDIOF_SETTIMEOUT | WDIOF_KEEPALIVEPING | WDIOF_MAGICCLOSE)
 
-static const struct watchdog_info sirfsoc_wdt_ident = {
+static const struct watchdog_info asm9260_wdt_ident = {
 	.options          =     OPTIONS,
 	.firmware_version =	0,
 	.identity         =	"SiRFSOC Watchdog",
 };
 
-static struct watchdog_ops sirfsoc_wdt_ops = {
+static struct watchdog_ops asm9260_wdt_ops = {
 	.owner = THIS_MODULE,
-	.start = sirfsoc_wdt_enable,
-	.stop = sirfsoc_wdt_disable,
-	.get_timeleft = sirfsoc_wdt_gettimeleft,
-	.ping = sirfsoc_wdt_updatetimeout,
-	.set_timeout = sirfsoc_wdt_settimeout,
+	.start = asm9260_wdt_enable,
+	.stop = asm9260_wdt_disable,
+	.get_timeleft = asm9260_wdt_gettimeleft,
+	.ping = asm9260_wdt_updatetimeout,
+	.set_timeout = asm9260_wdt_settimeout,
 };
 
-static struct watchdog_device sirfsoc_wdd = {
-	.info = &sirfsoc_wdt_ident,
-	.ops = &sirfsoc_wdt_ops,
+static struct watchdog_device asm9260_wdd = {
+	.info = &asm9260_wdt_ident,
+	.ops = &asm9260_wdt_ops,
 	.timeout = SIRFSOC_WDT_DEFAULT_TIMEOUT,
 	.min_timeout = SIRFSOC_WDT_MIN_TIMEOUT,
 	.max_timeout = SIRFSOC_WDT_MAX_TIMEOUT,
 };
 
-static int sirfsoc_wdt_probe(struct platform_device *pdev)
+static int asm9260_wdt_probe(struct platform_device *pdev)
 {
 	struct resource *res;
 	int ret;
@@ -150,40 +173,40 @@ static int sirfsoc_wdt_probe(struct platform_device *pdev)
 	if (IS_ERR(base))
 		return PTR_ERR(base);
 
-	watchdog_set_drvdata(&sirfsoc_wdd, base);
+	watchdog_set_drvdata(&asm9260_wdd, base);
 
-	watchdog_init_timeout(&sirfsoc_wdd, timeout, &pdev->dev);
-	watchdog_set_nowayout(&sirfsoc_wdd, nowayout);
+	watchdog_init_timeout(&asm9260_wdd, timeout, &pdev->dev);
+	watchdog_set_nowayout(&asm9260_wdd, nowayout);
 
-	ret = watchdog_register_device(&sirfsoc_wdd);
+	ret = watchdog_register_device(&asm9260_wdd);
 	if (ret)
 		return ret;
 
-	platform_set_drvdata(pdev, &sirfsoc_wdd);
+	platform_set_drvdata(pdev, &asm9260_wdd);
 
 	return 0;
 }
 
-static void sirfsoc_wdt_shutdown(struct platform_device *pdev)
+static void asm9260_wdt_shutdown(struct platform_device *pdev)
 {
 	struct watchdog_device *wdd = platform_get_drvdata(pdev);
 
-	sirfsoc_wdt_disable(wdd);
+	asm9260_wdt_disable(wdd);
 }
 
-static int sirfsoc_wdt_remove(struct platform_device *pdev)
+static int asm9260_wdt_remove(struct platform_device *pdev)
 {
-	sirfsoc_wdt_shutdown(pdev);
+	asm9260_wdt_shutdown(pdev);
 	return 0;
 }
 
 #ifdef	CONFIG_PM_SLEEP
-static int sirfsoc_wdt_suspend(struct device *dev)
+static int asm9260_wdt_suspend(struct device *dev)
 {
 	return 0;
 }
 
-static int sirfsoc_wdt_resume(struct device *dev)
+static int asm9260_wdt_resume(struct device *dev)
 {
 	struct watchdog_device *wdd = dev_get_drvdata(dev);
 
@@ -192,35 +215,33 @@ static int sirfsoc_wdt_resume(struct device *dev)
 	 * and restored back by the timer-prima2.c, so we need not
 	 * update WD settings except refreshing timeout.
 	 */
-	sirfsoc_wdt_updatetimeout(wdd);
+	asm9260_wdt_updatetimeout(wdd);
 
 	return 0;
 }
 #endif
 
-static SIMPLE_DEV_PM_OPS(sirfsoc_wdt_pm_ops,
-		sirfsoc_wdt_suspend, sirfsoc_wdt_resume);
+static SIMPLE_DEV_PM_OPS(asm9260_wdt_pm_ops,
+		asm9260_wdt_suspend, asm9260_wdt_resume);
 
-static const struct of_device_id sirfsoc_wdt_of_match[] = {
-	{ .compatible = "sirf,prima2-tick"},
+static const struct of_device_id asm9260_wdt_of_match[] = {
+	{ .compatible = "alphascale,asm9260-wdt"},
 	{},
 };
-MODULE_DEVICE_TABLE(of, sirfsoc_wdt_of_match);
+MODULE_DEVICE_TABLE(of, asm9260_wdt_of_match);
 
-static struct platform_driver sirfsoc_wdt_driver = {
+static struct platform_driver asm9260_wdt_driver = {
 	.driver = {
-		.name = "sirfsoc-wdt",
+		.name = "asm9260-wdt",
 		.owner = THIS_MODULE,
-		.pm = &sirfsoc_wdt_pm_ops,
-		.of_match_table	= sirfsoc_wdt_of_match,
+		.pm = &asm9260_wdt_pm_ops,
+		.of_match_table	= asm9260_wdt_of_match,
 	},
-	.probe = sirfsoc_wdt_probe,
-	.remove = sirfsoc_wdt_remove,
-	.shutdown = sirfsoc_wdt_shutdown,
+	.probe = asm9260_wdt_probe,
+	.remove = asm9260_wdt_remove,
+	.shutdown = asm9260_wdt_shutdown,
 };
-module_platform_driver(sirfsoc_wdt_driver);
+module_platform_driver(asm9260_wdt_driver);
 
-MODULE_DESCRIPTION("SiRF SoC watchdog driver");
-MODULE_AUTHOR("Xianglong Du <Xianglong.Du@csr.com>");
+MODULE_AUTHOR("Oleksij Rempel <linux@rempel-privat.de>");
 MODULE_LICENSE("GPL v2");
-MODULE_ALIAS("platform:sirfsoc-wdt");

920d9ac17f817ec35bc6b8b06d56de58966a324a
Oleksij Rempel <linux@rempel-privat.de>
2015-02-04 12:02:43 +0100

    drivers/gpio/gpio-asm9260.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/gpio/gpio-asm9260.c
+++ drivers/gpio/gpio-asm9260.c
@@ -66,7 +66,6 @@ enum asm9260_gpios {
 
 struct asm9260_gpio {
 	struct gpio_chip chip;
-	struct asm9260 *asm9260;
 	u16 input_mask;		/* 1 = GPIO is input direction, 0 = output */
 };
 
@@ -77,135 +76,63 @@ static inline struct asm9260_gpio *to_asm9260_gpio(struct gpio_chip *_chip)
 
 static int asm9260_gpio_request(struct gpio_chip *chip, unsigned offset)
 {
-	struct asm9260_gpio *asm9260_gpio = to_asm9260_gpio(chip);
-	struct asm9260 *asm9260 = asm9260_gpio->asm9260;
-
-	/* Return an error if the pin is already assigned */
-	if (test_and_set_bit(offset, &asm9260->pin_used))
-		return -EBUSY;
-
+	printk("%s:%i\n", __func__, __LINE__);
 	return 0;
 }
 
 static void asm9260_gpio_free(struct gpio_chip *chip, unsigned offset)
 {
-	struct asm9260_gpio *asm9260_gpio = to_asm9260_gpio(chip);
-	struct asm9260 *asm9260 = asm9260_gpio->asm9260;
+	printk("%s:%i\n", __func__, __LINE__);
+	return 0;
 
-	clear_bit(offset, &asm9260->pin_used);
 }
 
 static int asm9260_gpio_set_mode(struct asm9260_gpio *asm9260_gpio, u8 offset,
 				u8 val)
 {
-	struct asm9260 *asm9260 = asm9260_gpio->asm9260;
-	const struct asm9260_reg_cfg *mux = asm9260->mux_cfg;
-
-	return asm9260_update_bits(asm9260, mux[offset].reg, mux[offset].mask,
-				  val << mux[offset].shift);
+	printk("%s:%i\n", __func__, __LINE__);
+	return 0;
 }
 
 static int asm9260_gpio_direction_input(struct gpio_chip *chip, unsigned offset)
 {
-	struct asm9260_gpio *asm9260_gpio = to_asm9260_gpio(chip);
-
-	asm9260_gpio->input_mask |= BIT(offset);
-
-	return asm9260_gpio_set_mode(asm9260_gpio, offset, ASM9260_GPIO_IN);
+	printk("%s:%i\n", __func__, __LINE__);
+	return 0;
 }
 
 static int asm9260_get_gpio_in_status(struct asm9260_gpio *asm9260_gpio,
 				     struct gpio_chip *chip, unsigned offset)
 {
-	u8 addr, read;
-	int err;
-
-	switch (offset) {
-	case ASM9260_GPIO1 ... ASM9260_GPIO8:
-		addr = ASM9260_REG_GPIO_A;
-		break;
-	case ASM9260_GPIO9 ... ASM9260_GPIO16:
-		addr = ASM9260_REG_GPIO_B;
-		offset = offset - 8;
-		break;
-	default:
-		return -EINVAL;
-	}
-
-	err = asm9260_read_byte(asm9260_gpio->asm9260, addr, &read);
-	if (err)
-		return err;
-
-	return !!(read & BIT(offset));
+	printk("%s:%i\n", __func__, __LINE__);
+	return 0;
 }
 
 static int asm9260_get_gpio_out_status(struct asm9260_gpio *asm9260_gpio,
 				      struct gpio_chip *chip, unsigned offset)
 {
-	struct asm9260 *asm9260 = asm9260_gpio->asm9260;
-	const struct asm9260_reg_cfg *mux = asm9260->mux_cfg;
-	u8 read;
-	int err;
-
-	err = asm9260_read_byte(asm9260, mux[offset].reg, &read);
-	if (err)
-		return err;
-
-	read = (read & mux[offset].mask) >> mux[offset].shift;
-
-	if (read == ASM9260_GPIO_OUT_HIGH)
-		return 1;
-	else if (read == ASM9260_GPIO_OUT_LOW)
-		return 0;
-	else
-		return -EINVAL;
+	printk("%s:%i\n", __func__, __LINE__);
+	return 0;
 }
 
 static int asm9260_gpio_get(struct gpio_chip *chip, unsigned offset)
 {
-	struct asm9260_gpio *asm9260_gpio = to_asm9260_gpio(chip);
-
-	/*
-	 * Limitation:
-	 *   ASM9260 doesn't have the GPIO direction register. It provides
-	 *   only input and output status registers.
-	 *   So, direction info is required to handle the 'get' operation.
-	 *   This variable is updated whenever the direction is changed and
-	 *   it is used here.
-	 */
-
-	if (asm9260_gpio->input_mask & BIT(offset))
-		return asm9260_get_gpio_in_status(asm9260_gpio, chip, offset);
-	else
-		return asm9260_get_gpio_out_status(asm9260_gpio, chip, offset);
+	printk("%s:%i\n", __func__, __LINE__);
 }
 
 static void asm9260_gpio_set(struct gpio_chip *chip, unsigned offset, int value)
 {
-	struct asm9260_gpio *asm9260_gpio = to_asm9260_gpio(chip);
-	u8 data;
-
-	if (value)
-		data = ASM9260_GPIO_OUT_HIGH;
-	else
-		data = ASM9260_GPIO_OUT_LOW;
-
-	asm9260_gpio_set_mode(asm9260_gpio, offset, data);
+	printk("%s:%i\n", __func__, __LINE__);
 }
 
 static int asm9260_gpio_direction_output(struct gpio_chip *chip, unsigned offset,
 					int value)
 {
-	struct asm9260_gpio *asm9260_gpio = to_asm9260_gpio(chip);
-
-	asm9260_gpio_set(chip, offset, value);
-	asm9260_gpio->input_mask &= ~BIT(offset);
-
+	printk("%s:%i\n", __func__, __LINE__);
 	return 0;
 }
 
 static const struct gpio_chip asm9260_gpio_chip = {
-	.label			= "asm9260",
+	.label			= "asm9260-gpio",
 	.owner			= THIS_MODULE,
 	.request		= asm9260_gpio_request,
 	.free			= asm9260_gpio_free,
@@ -220,7 +147,6 @@ static const struct gpio_chip asm9260_gpio_chip = {
 
 static int asm9260_gpio_probe(struct platform_device *pdev)
 {
-	struct asm9260 *asm9260 = dev_get_drvdata(pdev->dev.parent);
 	struct asm9260_gpio *asm9260_gpio;
 
 	asm9260_gpio = devm_kzalloc(&pdev->dev, sizeof(*asm9260_gpio),
@@ -228,7 +154,6 @@ static int asm9260_gpio_probe(struct platform_device *pdev)
 	if (!asm9260_gpio)
 		return -ENOMEM;
 
-	asm9260_gpio->asm9260 = asm9260;
 	asm9260_gpio->chip = asm9260_gpio_chip;
 	asm9260_gpio->chip.dev = &pdev->dev;
 
